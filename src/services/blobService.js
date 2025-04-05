68fbe0d785e8f5ee8bcd6baabb1a347bfa1fc5c5
whereismytime <a.v.dyma@gmail.com>
2025-04-05 14:46:28 +0100

    fix: use absolute API URL in production

--- src/services/blobService.js
+++ src/services/blobService.js
@@ -1,11 +1,25 @@
 import axios from 'axios';
 
-const api = '/api/BlobStorage';
+// 👇 Автоматический выбор адреса API в зависимости от среды
+const api = import.meta.env.PROD
+  ? 'https://blobsapp-dddrgbehh2fsdphw.canadacentral-01.azurewebsites.net/api/BlobStorage'
+  : '/api/BlobStorage';
 
 export const getContainers = () => axios.get(`${api}/GetContainers`);
-export const getBlobFiles = (container) => axios.get(`${api}/GetBlobFiles`, { params: { containerName: container } });
-export const downloadBlob = (container, file) => axios.get(`${api}/DownloadBlobFile`, { params: { containerName: container, fileName: file }, responseType: 'blob' });
-export const deleteBlob = (container, file) => axios.delete(`${api}/DeleteBlobFile`, { params: { containerName: container, fileName: file } });
+export const getBlobFiles = (container) =>
+  axios.get(`${api}/GetBlobFiles`, { params: { containerName: container } });
+
+export const downloadBlob = (container, file) =>
+  axios.get(`${api}/DownloadBlobFile`, {
+    params: { containerName: container, fileName: file },
+    responseType: 'blob',
+  });
+
+export const deleteBlob = (container, file) =>
+  axios.delete(`${api}/DeleteBlobFile`, {
+    params: { containerName: container, fileName: file },
+  });
+
 export const uploadBlob = (container, file) => {
   const formData = new FormData();
   formData.append('file', file);
